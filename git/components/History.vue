ed23e8eb6065003eef7e83dc9ecfa54d4dd5857d
pouya3883 <pouya3883@gmail.com>
2022-07-07 13:29:09 +0430

    Showing History

--- git/components/History.vue
+++ git/components/History.vue
@@ -1,13 +1,15 @@
 <template>
-  
+  <div>
+    <span>{{ resultObject.userName }} : {{ resultObject.sec }}</span>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    resultObject: Object,
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style scoped></style>
