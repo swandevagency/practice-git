a095a97f1a47340bc4f6ac653beb701b89280ad9
pouya3883 <pouya3883@gmail.com>
2022-07-07 11:54:35 +0430

    History and ClickMeButton imported into the ClickMeBox.vue, styles added

--- git/components/ClickMeBox.vue
+++ git/components/ClickMeBox.vue
@@ -1,13 +1,38 @@
 <template>
-  
+  <div id="box">
+    <ClickMeButton />
+    <div id="history">
+      <ul>
+        <History />
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import ClickMeButton from '../components/ClickMeButton.vue'
+import History from './History.vue'
 export default {
-
+  data() {
+    return {}
+  },
+  components: {
+    ClickMeButton,
+    History,
+  },
 }
 </script>
 
 <style>
+#box {
+  height: 28rem;
+  border: 1px solid rgb(236, 236, 236);
+  box-shadow: 0 0 16px rgba(109, 109, 109, 0.2);
+  border-radius: 12px;
+}
 
+#history {
+  position: relative;
+  top: 630px;
+}
 </style>
