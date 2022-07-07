e42f8586da5eb94f6b3d4dd36676fe628322c732
pouya3883 <pouya3883@gmail.com>
2022-07-07 13:05:15 +0430

    StartButton's element & its styles

--- git/components/StartButton.vue
+++ git/components/StartButton.vue
@@ -1,5 +1,7 @@
 <template>
-  
+  <div>
+    <button>Start</button>
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,23 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+button {
+  position: relative;
+  left: 1338px;
+  bottom: 100px;
+  font-size: 16px;
+  color: white;
+  background-color: rgb(134, 134, 134);
+  border: 1px solid rgb(134, 134, 134);
+  padding: 12px 36px;
+  border-radius: 10px;
+  cursor: pointer;
+  box-shadow: 0 0 17px rgba(0, 0, 0, 0.2);
+}
 
+button:hover {
+  background-color: rgb(150, 150, 150);
+  border: 1px solid rgb(150, 150, 150);
+}
 </style>
