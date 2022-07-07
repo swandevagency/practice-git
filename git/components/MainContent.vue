368f9a4245e5ac768be1ab5056a7b41e6b9aae38
pouya3883 <pouya3883@gmail.com>
2022-07-07 11:49:07 +0430

    ClickMeBox, NameInput & StartButton imported into the MainContent.vue

--- git/components/MainContent.vue
+++ git/components/MainContent.vue
@@ -1,13 +1,30 @@
 <template>
-  
+  <div class="main-content-component-wrapper" id="mainContent">
+    <ClickMeBox />
+    <NameInput />
+    <StartButton />
+  </div>
 </template>
 
 <script>
+import ClickMeBox from '../components/ClickMeBox.vue'
+import NameInput from '../components/NameInput.vue'
+import StartButton from '../components/StartButton.vue'
 export default {
-
+  data() {
+    return {}
+  },
+  components: {
+    ClickMeBox,
+    NameInput,
+    StartButton,
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+#mainContent {
+  width: 76%;
+  margin: 150px auto 100px auto;
+}
 </style>
