ed829a622f08d8977fb7cabd00e6eae14db2c7e6
pouya3883 <pouya3883@gmail.com>
2022-07-07 12:03:48 +0430

    ClickMeButton Styles

--- git/components/ClickMebutton.vue
+++ git/components/ClickMebutton.vue
@@ -1,13 +1,26 @@
 <template>
-  
+  <div>
+    <button id="clickMe">Click Me!</button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {}
+  },
 }
 </script>
 
 <style>
-
+button {
+  position: relative;
+  color: white;
+  padding: 13px 26px;
+  background-color: rgb(97, 95, 95);
+  border: 1px solid rgb(97, 95, 95);
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.5);
+  border-radius: 8px;
+  cursor: pointer;
+}
 </style>
